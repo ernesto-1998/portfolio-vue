09e5173607967c4a43a990f29a835d16820f248d
ernesto-1998 <ernestoalonso1998@gmail.com>
2024-12-14 22:07:20 -0600

    Updating work experience order

--- src/utils/work.js
+++ src/utils/work.js
@@ -1,20 +1,20 @@
 const works = [
   {
-    company: 'GoLance inc.',
-    position: 'Frontend Developer',
-    duration: 'Jul. 2023 - Sept. 2023 (Intership)',
-    tech: 'Html, Css, Javascript, Tailwind, Vue 2, Vuex, Uikit, handlebars, git, github flow',
-    description:
-      'I worked on a wide variety of tasks related to the frontend of the golance core app. From updating the apps email service templates to new designs, resolving visual bugs that could range from a title that overflowed in a form, to more complex bugs such as components that did not respond correctly, and I was also in charge of migrating complete pages of the application to a more modern design (Updating components and other things).'
-  },
-  {
     company: 'Clearview',
     position: 'Software Developer',
     duration: 'Jun. 2024 - Present',
     tech: 'Angular, Typescript, HTML, CSS, C#, .NET, MongoDB, SQL Server, SASS, Git',
     description:
       'I am responsible for maintaining and enhancing the company\'s core software, addressing bugs to ensure smooth functionality, and implementing new features based on business and user requirements. My role involves collaborating with the team to improve code quality and optimize system performance.'
-  }  
+  }, 
+  {
+    company: 'GoLance inc.',
+    position: 'Frontend Developer',
+    duration: 'Jul. 2023 - Sept. 2023 (Intership)',
+    tech: 'Html, Css, Javascript, Tailwind, Vue 2, Vuex, Uikit, handlebars, git, github flow',
+    description:
+      'I worked on a wide variety of tasks related to the frontend of the golance core app. From updating the apps email service templates to new designs, resolving visual bugs that could range from a title that overflowed in a form, to more complex bugs such as components that did not respond correctly, and I was also in charge of migrating complete pages of the application to a more modern design (Updating components and other things).'
+  }, 
 ]
 
 export default works
