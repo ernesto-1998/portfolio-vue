da4958edb579de990e1a353c9bdf7679554b02e2
ernesto-1998 <ernestoalonso1998@gmail.com>
2025-06-08 12:34:15 -0600

    Works order conflict fix

--- src/utils/work.js
+++ src/utils/work.js
@@ -1,9 +1,14 @@
 const works = [
   {
-<<<<<<< Updated upstream
-    company: 'Clearview',
-    position: 'Software Developer',
-=======
+    company: 'Nice Ltd.',
+    position: 'Software Engineer',
+    duration: 'Jun. 2024 - Present',
+    tech: 'Angular, Typescript, HTML, CSS, C#, .NET, MongoDB, SQL Server, SASS, Git, Postgres',
+    description:
+      'I am responsible for maintaining and enhancing the company\'s core software, addressing bugs to ensure smooth functionality, and implementing new features based on business and user requirements. My role involves collaborating with the team to improve code quality and optimize system performance.',
+    active: true,
+  },  
+  {
     company: 'GoLance inc.',
     position: 'Frontend Developer',
     duration: 'Jul. 2023 - Sept. 2023 (Intership)',
@@ -12,29 +17,6 @@ const works = [
       'I worked on a wide variety of tasks related to the frontend of the golance core app. From updating the apps email service templates to new designs, resolving visual bugs that could range from a title that overflowed in a form, to more complex bugs such as components that did not respond correctly, and I was also in charge of migrating complete pages of the application to a more modern design (Updating components and other things).',
     active: false,
   },
-  {
-    company: 'Nice Ltd.',
-    position: 'Software Engineer',
->>>>>>> Stashed changes
-    duration: 'Jun. 2024 - Present',
-    tech: 'Angular, Typescript, HTML, CSS, C#, .NET, MongoDB, SQL Server, SASS, Git, Postgres',
-    description:
-<<<<<<< Updated upstream
-      'I am responsible for maintaining and enhancing the company\'s core software, addressing bugs to ensure smooth functionality, and implementing new features based on business and user requirements. My role involves collaborating with the team to improve code quality and optimize system performance.'
-  }, 
-  {
-    company: 'GoLance inc.',
-    position: 'Frontend Developer',
-    duration: 'Jul. 2023 - Sept. 2023 (Intership)',
-    tech: 'Html, Css, Javascript, Tailwind, Vue 2, Vuex, Uikit, handlebars, git, github flow',
-    description:
-      'I worked on a wide variety of tasks related to the frontend of the golance core app. From updating the apps email service templates to new designs, resolving visual bugs that could range from a title that overflowed in a form, to more complex bugs such as components that did not respond correctly, and I was also in charge of migrating complete pages of the application to a more modern design (Updating components and other things).'
-  }, 
-=======
-      'I am responsible for maintaining and enhancing the company\'s core software, addressing bugs to ensure smooth functionality, and implementing new features based on business and user requirements. My role involves collaborating with the team to improve code quality and optimize system performance.',
-    active: true,
-  }  
->>>>>>> Stashed changes
 ]
 
 export default works
